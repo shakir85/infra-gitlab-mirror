39ffb7a8f456ffb234fe9d636a4f5d45ab98105b
Shakir <sralmasaari@gmail.com>
2024-05-29 18:05:26 -0700

    Add logging VM

---
+++ terraform/pve1/logger/main.tf
@@ -0,0 +1,16 @@
+module "logger" {
+  source              = "git::https://github.com/shakir85/terraform_modules.git//proxmox/vm?ref=v0.0.3"
+  proxmox_node_name   = "pve1"
+  disk_name           = "sda"
+  ssh_public_key_path = var.id_rsa_pub
+  username            = "shakir"
+  hostname            = "logger"
+  timezone            = "UTC"
+  cloud_image_info    = ["sdc", "debian-12-generic-amd64.qcow2.img"]
+  tags                = ["prod", "debian12"]
+  description         = "Managed by Terraform. Logs management."
+  memory              = 4096
+  cores               = 2
+  sockets             = 1
+  disk_size           = 256
+}
